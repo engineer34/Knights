f6e6955a7769c9f6a7defd5ad184a0c4213ce26b
Javier Medina <130412345+engineer34@users.noreply.github.com>
2026-03-16 12:23:30 -0500

    Update Knights.js

--- Knights.js
+++ Knights.js
@@ -2,14 +2,18 @@
 class Knight{
     constructor(position, path = [position]){
         this.position = position;
+        // path = array of all squares visited to reach this position
         this.path = path;
     }
 }
 //our function that gets moves from x and y when we 
 //add or minus
 function getMoves([x,y]){
+    //all possible 8 moves
     const moves = [[x+1, y+2],[x+2, y+1],[x+2, y-1],
 [x+1, y-2],[x-1, y-2],[x-2, y-1],[x-2, y+1],[x-1, y+2],];
+    //ilter out moves that go off the board
+  // chess board is 8x8so coordinates stay between zero and seven
 return moves.filter(([a,b]) => a >= 0 && a < 8 && 
 b >= 0 && b < 8);
 }
@@ -17,10 +21,12 @@ b >= 0 && b < 8);
 function knightMoves(start, end) {
     const queue = [new Knight(start)];
     const visited = new Set();
+    //continues search while queue is emepty
     while (queue.length > 0) {
+        //remove first element in queue
     const current = queue.shift();
     const [x, y] = current.position;
-
+// Check if we reached the target square
     if (x === end[0] && y === end[1]) {
       console.log(`You made it in ${current.path.length - 1} moves!`);
       console.log("Path:");
@@ -31,7 +37,9 @@ function knightMoves(start, end) {
     visited.add(`${x},${y}`);
 
 for (const move of getMoves(current.position)){
+    // Only visit squares we haven't already explored
     if(!visited.has(`${move[0]}, ${move[1]}`)){
+        //sets new queue
         queue.push(new Knight(move, [...current.path, move]));
     }
 }
